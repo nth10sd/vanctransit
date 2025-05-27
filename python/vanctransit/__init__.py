d17ae2afc578f27cc6a8fd9c0de794c57baed17e
Gary Kwong [:gkw] <nth10sd@gmail.com>
2025-05-27 16:29:58 -0700

    Activate beartype on this package

--- python/vanctransit/__init__.py
+++ python/vanctransit/__init__.py
@@ -1,4 +1,17 @@
 """Module details."""
 
+import contextlib
+
+with contextlib.suppress(ModuleNotFoundError):  # Bypass package install issues on CI
+    from beartype import BeartypeConf
+    from beartype.claw import beartype_this_package
+
 __title__ = "vanctransit"
 __version__ = "0.1.0"
+
+with contextlib.suppress(NameError):
+    beartype_this_package(  # pyright: ignore[reportPossiblyUnboundVariable]
+        conf=BeartypeConf(  # pyright: ignore[reportPossiblyUnboundVariable]
+            violation_type=UserWarning
+        )
+    )
